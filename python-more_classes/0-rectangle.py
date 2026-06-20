bd59bc0593ffde7f4e8ace8f91e89559e03ee148
McGhaida <mcghaida@McGhaidas-Mac-mini.local>
2026-06-20 22:31:35 +0300

    initial commit

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Alsabti Ghaida <13216@holbertonschool.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks of more clsses project
