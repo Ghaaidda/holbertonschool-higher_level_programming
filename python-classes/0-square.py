72aea41d004e302772ddf81e57feee081782308c
McGhaida <mcghaida@McGhaidas-Mac-mini.local>
2026-06-20 22:36:04 +0300

    Fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+# 0-square.py
+# Alsabti Ghaida <13126@holbertonstudents.com>
 """Define a class square."""
 class Square:
     """Represensts a square."""
-    def __init__(self):
-        return
+    pass
