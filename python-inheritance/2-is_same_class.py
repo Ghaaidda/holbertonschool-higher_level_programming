897c42d2fa26a94b23f0c03ce032b79a9a65e7bd
Ghaida <alsabtighaida@gmail.com>
2026-06-27 18:23:41 +0000

    add tasks

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 # 2-is_same_class.py
-# Alsabti ghaida <13216@holbertstudenst.com>
-"""Defines an object attribute matching check funciton."""
+# alsabti ghaida
+"""Defines a class-checking function."""
+
 
 def is_same_class(obj, a_class):
-    """Checks if the object is exactly an instance of the specified class.
-       Args:
+    """Check if an object is exactly an instance of a given class.
+
+    Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
-        Returns:
+    Returns:
         If obj is exactly an instance of a_class - True.
         Otherwise - False.
     """
-    if isinstance(obj, a_class)
+    if type(obj) == a_class:
         return True
-    else
-        return False                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-        
+    return False
---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# alsabti ghaida
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
