e8dd2ca3136338c56c0d543648791556ad1522cb
Ghaida <alsabtighaida@gmail.com>
2026-06-27 18:13:14 +0000

    fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This is my list module that contains a class MyList that inherits 
+This file contains a class MyList that inherits 
 from the built-in list class and adds a method to print the list in sorted order.
 """
 class MyList(list):
