c89d92be54b08258913bc36747b53b018b6c1e7d
Ghaida <alsabtighaida@gmail.com>
2026-06-27 18:03:14 +0000

    Fix

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,7 @@
 # Alsabti ghaida <13216@holbertstudenst.com>
 """Defines an object attribute lookup function."""
 
+
 def lookup(obj):
     """Return a list of an object's available attributes."""
     return (dir(obj))
