edca9489873a7db8b145801582d8b2eccde93b8d
Ghaida <alsabtighaida@gmail.com>
2026-07-11 18:33:08 +0000

    fixed messages format/wording

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -25,15 +25,21 @@ class SimpleAPI_Handler(http.server.BaseHTTPRequestHandler):
         
         elif self.path == "/status":
             self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        elif self.path == "/info":
+            self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(b'{"status": "OK"}')
+            self.wfile.write(b'{"version": "1.0", "description": "A simple API built with http.server"}')
 
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 if __name__ == "__main__":
